9e4058b8a2abb48dec785a73010212b1e3e471de
Petrov Vadim <vadikdydy@mail.ru>
2025-12-01 17:16:58 +0300

    DOT add

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ namespace top
 
   struct IDraw
   {
-    virtual p_t begin() = 0;
-    virtual p_t next(p_t) = 0;
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
   };
 
   bool operator==(p_t a, p_t b)
@@ -20,12 +20,31 @@ namespace top
   {
     return !(a == b);
   }
-}
 
+  struct Dot: IDraw
+  {
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
+  };
+  
+}
 
 
 int main()
 {
   using namespace top;
-  
+
+}
+
+top::Dot::Dot(int x, int y):
+  IDraw(),
+  o{x, y}
+{}
+top::p_t top::Dot::begin() const{
+  return o;
+}
+top::p_t top::Dot::next(p_t) const{
+  return begin();
 }
