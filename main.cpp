ba85af1fb135b266f0c8d56aa01ccba5f00ee183
Petrov Vadim <vadikdydy@mail.ru>
2025-12-01 17:00:16 +0300

    IDraw interface

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
+namespace top
+{
+  struct p_t
+  {
+    int x, y;
+  };
+
+  struct IDraw
+  {
+    virtual p_t begin() = 0;
+    virtual p_t next(p_t) = 0;
+  };
+
+  bool operator==(p_t a, p_t b)
+  {
+    return a.x == b.x && a.y == b.y;
+  }
+
+  bool operator!=(p_t a, p_t b)
+  {
+    return !(a == b);
+  }
+}
+
+
+
 int main()
 {
+  using namespace top;
   
 }
