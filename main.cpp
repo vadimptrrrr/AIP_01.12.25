9871d7dcb3abf4108bc3ca012b5e7ab35adb8886
Petrov Vadim <vadikdydy@mail.ru>
2025-12-01 23:22:23 +0300

    lesson was hard...

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+
 namespace top
 {
   struct p_t
@@ -29,13 +31,74 @@ namespace top
     Dot(int x, int y);
   };
   
+  struct frame_t
+  {
+    p_t left_bot;
+    p_t right_top;
+  };
+  
+  void make_f(IDraw** b, size_t k)
+  {
+
+  }
+
+  void get_points(IDraw* b, p_t** ps, size_t& s)
+  {
+
+  }
+
+  frame_t build_frame(const p_t* ps, size_t s)
+  {
+
+  }
+
+  char* build_canvas(frame_t f)
+  {
+
+  }
+
+  void paint_canvas(char* cnv, frame_t fr, const p_t* ps, size_t k, char f)
+  {
+
+  }
+
+  void print_canvas(const char* cnv, frame_t fr)
+  {
+
+  }
 }
 
 
 int main()
 {
   using namespace top;
+  int err = 0;
+  IDraw* f[3] = {};
+  char* cnv = nullptr;
+  p_t* p = nullptr;
+  p_t** kp = nullptr;
 
+  try
+  {
+    make_f(f, 3);
+    size_t s = 0;
+    for(size_t i =0; i < 3; ++i)
+    {
+      get_points(f[i], kp, s);
+    }
+    frame_t fr = build_frame(p, s);
+    cnv = build_canvas(fr);
+    paint_canvas(cnv, fr, p, s, '#');
+    print_canvas(cnv, fr);
+  } catch (...) {
+    err = 1;
+    delete f[0];
+    delete f[1];
+    delete f[2];
+    delete[] p;
+    delete[] cnv;
+    return err;
+  }
 }
 
 top::Dot::Dot(int x, int y):
